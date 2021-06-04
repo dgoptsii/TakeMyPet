bff50e78b7a8ed25006b2052d25d1b45760e3562
KateKolokhina <39592816+KateKolokhina@users.noreply.github.com>
2021-06-04 21:40:03 +0300

    logout add

--- src/main/java/com/naukma/practice/myPet/controllers/MainController.java
+++ src/main/java/com/naukma/practice/myPet/controllers/MainController.java
@@ -12,8 +12,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.security.sasl.AuthenticationException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @Controller
@@ -27,6 +29,20 @@ public class MainController {
     public String helloPage(){
         log.info("hello");
         return "index";
+    }
+
+    @GetMapping(path = {"/logout"})
+    public void execute(HttpSession session, HttpServletResponse response, HttpServletRequest request)
+            throws IOException {
+
+        if (session != null)
+            session.invalidate();
+        else {
+            throw new AuthenticationException("No started sessions found");
+        }
+
+        log.debug("Command finished");
+        response.sendRedirect(request.getContextPath() +"/login");
     }
 
     @RequestMapping(value = "/", method = {RequestMethod.GET, RequestMethod.POST})
